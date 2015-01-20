83371b01db907e210a2725a6c3901c5f75a7aed0
hmorv <hugo.moragues@gmail.com>
2015-01-20 17:05:38 +0100

    eliminacion ficheros temporales
    index.php: modificada cadena a mostrar si usuario logueado o no.

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ if(isset($_SESSION['id'])) {
 		
 	}
 	else {
-		echo "Logged as guest.";
+		echo "You are not logged in.";
 	}
 ?>
 </span>
 		<h1>Welcome to LinkKeeper</h1>
 		<p><em>LinkKeeper</em> is the funniest way to keep, share and manage your favourite bookmarks. Share linked information with other users.</p>
-		<p>For now, this application is only available for a few, who are known as 'the chosen'. If you want to be part of this elite, please, contact us <a href="mailto:none@mailkeeper.com?subject="LinkKeeper%20Account">here</a>
+		<p>For now, this application is only available for a few, who are known as 'the chosen'. If you want to be part of this elite, please, contact us <a href="mailto:none@mailkeeper.com?subject=LinkKeeper%20Account">here</a>
 
 <?php
 include('inc/footer.html');
