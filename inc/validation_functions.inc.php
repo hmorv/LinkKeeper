6d8e5d2a202dfbab585c9c96322472ec76172d40
hmorv <hugo.moragues@gmail.com>
2015-01-27 19:12:03 +0100

    register.php
    se crea nuevo fichero: validation_functions.inc.php contiene las
    funciones necesarias para validar formulario, comprobar email y
    username existente. aun no está terminado.
    
    en el SQL he modificado la tabla Users, ahora el campo Name admite
    hasta 20 caracteres.

--- inc/validation_functions.inc.php
+++ inc/validation_functions.inc.php
@@ -1,11 +1,27 @@
 <?php
-function validateMail(mail) {
+function validateMail($m) {
+	$pattern = '/[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}/';
+	return preg_match($pattern, $m);
+}
+function validateUserName($u) {
+	$pattern = '/^\w{3,20}$/';
+	return preg_match($pattern, $u);
+	}
+}
+function validatePass($p) {
+	/*this is quite tricky...
+	minimum length: & characters, maximum length: 20.
+	user must password must check:
+		-1 lowercase character
+		-1 uppercase character
+	*/
+	$pattern = '((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})';
+	return preg_match($pattern, $p);
+}
+function checkDBName($mail) {
 	return null;
 }
-function validateUserName(username) {
-	return null;
-}
-function validatePass(pass) {
+function checkDBUser($username) {
 	return null;
 }
 
--- register.php
+++ register.php
@@ -4,6 +4,8 @@ if(isset($_SESSION['id'])) {
 	redirect_user('main.php');
 	exit();
 }
+else {
+
 /*PENDIENTE:
 	Validacion campos del formulario...(funciones encapsuladas en otro php)
 	Procesar formulario (misma pagina, claro está....)
@@ -13,37 +15,55 @@ if(isset($_SESSION['id'])) {
 */
 	$page_title = "Register";
 	include('inc/header.html');
+	require('inc/validation_functions.inc.php');
+	$mail = NULL;
+	$username = NULL;
+	$pass = NULL;
 	if($_SERVER['METHOD']=='POST') {
+		$errors = array();
+		$mail = trim($_POST['formMail']);
+		$username =trim($_POST['formUser']);
+		$pass =;trim($_POST['formPass']);
 		require('inc/validation_functions.inc.php');
-
-		//if(validate()) {
-		//insertUser();
 		
+		if(validateMail($mail) && validateUserName($username) && validatePass($pass)) {
+			echo "validado!";
+			if(checkDBMail($mail) && checkDBUser($username)) {
+				insDB($user);
+			}
+			else {
+				showError($email,$userName);
+			}
+		}
+		else {
+			echo "no validado!";
+			//showError($registerForm);
+		}
 	}
-		//enviar datos
-		//lectura resultados
-		//visualizacion resultados
+?>
+<div id="login">
+	<form name="register" method="POST" action="register.php">
+		<table>
+			<tr>
+				<td>e-Mail account</td>
+				<td><input name="formMail" type="text"  maxlength="70"/></td>
+			</tr>
+			<tr>
+				<td>Bookmarker Name</td>
+				<td><input name="formUser" type="text"  maxlength="10"/></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input name="formPass" type="password"  maxlength="20"/></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="send"></input></td>
+			</tr>
+		</table>
+	</form>
+</div>
+<?php include('inc/footer.html');?>
 
-	} ?>
-	<div id="login">
-		<form name="register" method="POST" action="register.php">
-			<table>
-				<tr>
-					<td>e-Mail account</td>
-					<td><input name="regemail" type="text"  maxlength="70"/></td>
-				</tr>
-				<tr>
-					<td>Bookmarker Name</td>
-					<td><input name="reguser" type="text"  maxlength="10"/></td>
-				</tr>
-				<tr>
-					<td>Password</td>
-					<td><input name="regpass" type="password"  maxlength="20"/></td>
-				</tr>
-				<tr>
-					<td colspan="2"><input type="submit" value="send"></input></td>
-				</tr>
-			</table>
-		</form>
-	</div>
-	<?php include('inc/footer.html');?>
+
+
+
